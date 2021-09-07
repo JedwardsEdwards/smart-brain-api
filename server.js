1e02dfe4d63d221bd02a252a4ddf36d6c59d39b0
JedwardsEdwards <joseph.edwards@aquaq.co.uk>
2021-09-07 19:48:08 +0100

    Adding database connection

--- server.js
+++ server.js
@@ -11,9 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-flexible-08710',
-      user : 'postgres',
-      database : 'smart-brain'
+      host : process.env.DATABASE_URL,
+      ssl: true
     }
   });
 
